192335823d8a6758fc901ee09dbb031fac59c729
omar-hossam <omarhossam50@proton.me>
2026-04-25 18:35:04 +0300

    feat: implement responsive Navbar

---
+++ frontend/_data/navigation.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { name: "Need help", iconName: 'first-aid', url: "/need-help" },
+  { name: "Donate", iconName: 'donate', url: "/requests" },
+  { name: "Hospital Portal", iconName: 'hospital', url: "/login-hospital" },
+  { name: "Email Us", iconName: 'envelope', url: "/email-us" }
+];
